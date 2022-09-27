121a16776d7f49d83ae77e855a4676ff2489c159
tranvantiendat <vianhlathe2@gmail.com>
2022-09-27 17:33:52 +0700

    Cài đặt babel-plugin-module-resolver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { faAccessibleIcon } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>helo worl</h1>
+      <FontAwesomeIcon icon={faAccessibleIcon}></FontAwesomeIcon>
     </div>
   );
 }
